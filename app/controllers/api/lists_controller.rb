aba6a80442f745ff235485a0fc52e8c059821752
Daylan Merritt <daylan.merritt@gmail.com>
2022-12-05 18:47:33 -0700

    routes, controller, db, and seed

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,37 @@
+class Api::ListsController < ApplicationController
+  before_action :set_list, only: [:show, :update, :destroy]
+
+  def index
+    render json: list.all
+  end
+
+  def show
+    render json: @list
+  end
+
+  def create
+      @list = List.new(list_params)
+    if @list.save
+      render json: @list
+    else
+      render json: { errors: @list.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @list.update(list_params)
+      render json: @list
+    else
+      render json: { errors: @list.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    render json: { message: 'list deleted' }
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
+end
